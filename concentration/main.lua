a63c44ef5c046c2954150e44b73990c804860936
Ryan Arana <ryan@ryanarana.com>
2014-08-17 10:00:22 -0700

    Got a window and square

---
+++ concentration/main.lua
@@ -0,0 +1,33 @@
+WORLD_RESOLUTION = {
+  x = 320,
+  y = 480
+}
+
+SCREEN_RESOLUTION = {
+  x = 2 * WORLD_RESOLUTION.x,
+  y = 2 * WORLD_RESOLUTION.y
+}
+
+-- Window set up
+MOAISim.openWindow( "Concentration", SCREEN_RESOLUTION.x, SCREEN_RESOLUTION.y )
+
+-- Viewport set up
+viewport = MOAIViewport.new()
+viewport:setSize( SCREEN_RESOLUTION.x, SCREEN_RESOLUTION.y )
+viewport:setScale( WORLD_RESOLUTION.x, WORLD_RESOLUTION.y )
+
+layer = MOAILayer2D.new()
+layer:setViewport( viewport )
+
+imageGfx = MOAIGfxQuad2D.new()
+imageGfx:setTexture( 'assets/tile_back.png' )
+imageGfx:setRect( -31, -31, 31, 31 )
+
+imageProp = MOAIProp2D.new()
+imageProp:setDeck( imageGfx )
+imageProp:setLoc( 0, 0 )
+
+layer:insertProp( imageProp )
+
+renderTable = { layer }
+MOAIRenderMgr.setRenderTable( renderTable )
